8296e0ee8792bc57698041f3b82bb81d69c03976
cullen1107 <cullen@pm.me>
2023-07-19 18:35:26 -0500

    basic js setup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("connected");
+window.addEventListener("DOMContentLoaded", () => {
+  const checkSpot = (col) => {
+    const colSpots = document.querySelectorAll(
+      `.board-spot[data-col="${col}"]`
+    );
+    console.log(colSpots);
+    for (let i = colSpots.length - 1; i >= 0; i--) {
+      if (!colSpots[i].classList.contains("filled")) {
+        colSpots[i].classList.add("filled");
+        break;
+      }
+    }
+  };
+
+  const boardSpots = document.querySelectorAll(".board-spot");
+  boardSpots.forEach((boardSpot) => {
+    boardSpot.addEventListener("click", () => {
+      checkSpot(boardSpot.dataset.col);
+    });
+  });
+});
