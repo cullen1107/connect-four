569f18f2d8929331671964c0f18ae62274690531
cullen1107 <cullen@pm.me>
2023-07-19 19:33:46 -0500

    swap player functionality

--- js/app.js
+++ js/app.js
@@ -1,21 +1,30 @@
 window.addEventListener("DOMContentLoaded", () => {
+  let isPlayerOne = true;
+
   const checkSpot = (col) => {
     const colSpots = document.querySelectorAll(
       `.board-spot[data-col="${col}"]`
     );
-    console.log(colSpots);
     for (let i = colSpots.length - 1; i >= 0; i--) {
       if (!colSpots[i].classList.contains("filled")) {
-        colSpots[i].classList.add("filled");
+        isPlayerOne
+          ? colSpots[i].classList.add("filled", "player-one")
+          : colSpots[i].classList.add("filled", "player-two");
+        isPlayerOne = !isPlayerOne;
         break;
       }
     }
+  };
+
+  const checkBoard = () => {
+    console.log("yo");
   };
 
   const boardSpots = document.querySelectorAll(".board-spot");
   boardSpots.forEach((boardSpot) => {
     boardSpot.addEventListener("click", () => {
       checkSpot(boardSpot.dataset.col);
+      checkBoard();
     });
   });
 });
